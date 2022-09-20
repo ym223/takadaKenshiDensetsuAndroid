02e80ffbc4263abb4a7da6e64091516874fa8675
Yume <poppym0232@gmail.com>
2022-09-20 15:48:58 +0900

    大まかなレイアウト完成

--- app/src/main/java/com/example/takadakenshidensetsu/view/DensetsuScreen.kt
+++ app/src/main/java/com/example/takadakenshidensetsu/view/DensetsuScreen.kt
@@ -1,21 +1,30 @@
 package com.example.takadakenshidensetsu.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.navigation.NavController
 import com.example.takadakenshidensetsu.R
 
 @Composable
 fun DensetsuScreen() {
     Scaffold {
-        Column {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally, // 横方向
+            verticalArrangement = Arrangement.Center,
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.takadakenshi),
-                contentDescription = null,
+                contentDescription = "Takada Kenshi face",
             )
+            Text(text = "伝説")
         }
     }
 }
--- app/src/main/java/com/example/takadakenshidensetsu/view/StartScreen.kt
+++ app/src/main/java/com/example/takadakenshidensetsu/view/StartScreen.kt
@@ -1,16 +1,24 @@
 package com.example.takadakenshidensetsu.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
-fun StartScreen(navController: NavController){
+fun StartScreen(navController: NavController) {
     Scaffold {
-        Column {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally, // 横方向
+            verticalArrangement = Arrangement.Center,
+        ) {
             Text(text = "高田健志伝説")
             Button(onClick = { navController.navigate("densestu") }) {
                 Text(text = "伝説を探す")
