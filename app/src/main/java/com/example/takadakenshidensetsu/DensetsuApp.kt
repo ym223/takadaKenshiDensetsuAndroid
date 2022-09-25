0257290b55ce31baf81b3465baad93dc43c94d59
Yume <poppym0232@gmail.com>
2022-09-25 15:53:35 +0900

    MainActivityの処理を分ける

---
+++ app/src/main/java/com/example/takadakenshidensetsu/DensetsuApp.kt
@@ -0,0 +1,11 @@
+package com.example.takadakenshidensetsu
+
+import androidx.compose.runtime.Composable
+import com.example.takadakenshidensetsu.ui.theme.TakadaKenshiDensetsuTheme
+
+@Composable
+fun DensetsuApp() {
+    TakadaKenshiDensetsuTheme {
+        BottomNavigation()
+    }
+}
--- app/src/main/java/com/example/takadakenshidensetsu/MainActivity.kt
+++ app/src/main/java/com/example/takadakenshidensetsu/MainActivity.kt
@@ -3,21 +3,12 @@ package com.example.takadakenshidensetsu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.ui.Modifier
-import com.example.takadakenshidensetsu.ui.theme.TakadaKenshiDensetsuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TakadaKenshiDensetsuTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    BottomNavigation()
-                }
-            }
+            DensetsuApp()
         }
     }
 }
