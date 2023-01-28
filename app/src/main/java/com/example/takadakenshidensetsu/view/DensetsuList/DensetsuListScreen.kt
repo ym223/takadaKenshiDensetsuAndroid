70e77d3139bc17721310419cf9e0d27fd7cd9b38
Yume <poppym0232@gmail.com>
2023-01-28 15:08:00 +0900

    optimize import

--- app/src/main/java/com/example/takadakenshidensetsu/view/DensetsuList/DensetsuListScreen.kt
+++ app/src/main/java/com/example/takadakenshidensetsu/view/DensetsuList/DensetsuListScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,8 +23,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.takadakenshidensetsu.model.Densetsu
-import com.example.takadakenshidensetsu.view.Densetsu.DensetsuViewModel
 
 @Composable
 fun DensetsuListRoute(
